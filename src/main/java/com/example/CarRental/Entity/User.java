beefc9421873e22677d3a03f585ff1eeaab292e2
lakshya <lakshya10171@gmail.com>
2025-09-02 21:27:41 +0530

    Create User Entity

---
+++ src/main/java/com/example/CarRental/Entity/User.java
@@ -0,0 +1,35 @@
+package com.example.CarRental.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Users")
+public class User {
+    // define the properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String fullName;
+    private String phoneNo;
+
+    @Enumerated(EnumType.STRING)
+    private String Role;
+
+
+}
